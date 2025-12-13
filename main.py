fbb776c5c9de5c590aaac4f9676df3494af7ac0b
lovenangelo <lovenangelo.dev@gmail.com>
2025-12-13 19:06:17 +0800

    feat: ai agent

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from functions.run_python_file import schema_run_python_file
 from functions.call_function import call_function
 
 load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
 
+api_key = os.environ.get("GEMINI_API_KEY")
 if api_key == None:
     raise RuntimeError("Gemini API Key is not set")
 
@@ -27,43 +27,49 @@ def main():
     parser.add_argument("user_prompt", type=str, help="User prompt")
     parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
-
+    
     messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     responses = []
-
-    response = client.models.generate_content(
-        model="gemini-flash-lite-latest",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=SYSTEM_PROMPT),
-    )
-
-    if response.usage_metadata == None:
-        raise RuntimeError("API Request failed")
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-    for candidate in response.candidates():
-        messages.append(candidate.content)
-
-    if response.function_calls != None:
-        for fc in response.function_calls:
-            result = call_function(fc, args.verbose)
-            if len(result.parts) == 0:
-                raise Exception("Error: something went wrong")
+    
+    for i in range(0, 20):
+        try:
+            response = client.models.generate_content(
+                model="gemini-flash-lite-latest",
+                contents=messages,
+                config=types.GenerateContentConfig(
+                    tools=[available_functions],
+                    system_instruction=SYSTEM_PROMPT),
+            )
+            
+            if response.usage_metadata == None:
+                raise RuntimeError("API Request failed")
+            
+            if args.verbose:
+                print(f"User prompt: {args.user_prompt}")
+                print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+                print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+            
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+            
+            if response.function_calls != None:
+                for fc in response.function_calls:
+                    result = call_function(fc, args.verbose)
+                    if len(result.parts) == 0:
+                        raise Exception("Error: something went wrong")
+                    else:
+                        new_message = types.Content(role='user', parts=result.parts)
+                        messages.append(new_message)
+                        responses.append(result.parts[0])
+                        if args.verbose:
+                            print(f"-> {result.parts[0].function_response.response}")
             else:
-                new_message = types.Content(role: 'user', result.parts)
-                messages.append(new_message)
-                responses.append(result.parts[0])
-                if args.verbose:
-                    print(f"-> {result.parts[0].function_response.response}")
-
-    else:
-        print(response.text)
-
+                print(response.text)
+                break
+                
+        except Exception as e:
+            print(f"Exception Error: {e}")
+            break
 
 if __name__ == "__main__":
     main()
